824d1334da1201ae304fdf59695e2d1a92af9738
Nguyen Anh Duy <aduy1912@gmail.com>
2024-05-22 21:44:04 +0700

    add constraint in creating product

--- controllers/productCategoryController.js
+++ controllers/productCategoryController.js
@@ -7,16 +7,16 @@ class ProductCategoryController {
                 res.status(200).json(categories)
             })
             .catch(err => {
-                res.status(500).json('Error:' + err.message)
+                res.status(500).json('Có lỗi xảy ra:' + err.message)
             })
     }
     async create(req, res, next) {
         ProductCategories.create(req.body)
             .then((category) => {
-                res.status(200).json('Product category created');
+                res.status(200).json('Tạo mới thành công loại sản phẩm');
             })
             .catch(err => {
-                res.status(500).json('Error:' + err.message)
+                res.status(500).json('Có lỗi xảy ra:' + err.message)
             })
     };
 }
--- controllers/productController.js
+++ controllers/productController.js
@@ -7,16 +7,22 @@ class ProductController {
                 res.status(200).json(products)
             })
             .catch(err => {
-                res.status(500).json('Error:' + err.message)
+                res.status(500).json('Có lỗi xảy ra:' + err.message)
             })
     }
     async create(req, res, next) {
+        if (req.body.isInShop) {
+            Products.findOne({ name: req.body.name, isInShop: true })
+                .then((products) => {
+                    res.status(500).json('Sản phẩm trùng tên!');
+            })
+        }
         Products.create(req.body)
             .then((product) => {
-                res.status(200).json('Product created');
+                res.status(200).json('Tạo sản phẩm thành công');
             })
             .catch(err => {
-                res.status(500).json('Error:' + err.message)
+                res.status(500).json('Có lỗi xảy ra:' + err.message)
             })
     };
 }
