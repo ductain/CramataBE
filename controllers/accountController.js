978abdf84e08800bc1ecec97b25d3cd6c2db0d8d
ductain <taindse170080@fpt.edu.vn>
2024-06-01 21:22:55 +0700

    create account for children

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,4 +1,5 @@
 const Accounts = require("../models/Account");
+const Points = require("../models/Point")
 const bcrypt = require("bcrypt");
 
 class AccountController {
@@ -40,6 +41,68 @@ class AccountController {
           });
         }
       });
+    }
+  }
+
+  async registerChild(req, res, next) {
+    try {
+      const {
+        username,
+        password,
+        firstName,
+        lastName,
+        image,
+        phone,
+        email,
+      } = req.body;
+
+      // Validate the input
+      if (!username || !password || !firstName || !lastName || !phone || !email) {
+        return res.status(400).json({ error: "Xin hãy nhập tất cả thông tin" });
+      }
+
+      if (password.length < 6) {
+        return res.status(400).json({ error: "Password phải có ít nhất 6 ký tự" });
+      }
+
+      // Check if the username already exists
+      const existingUser = await Accounts.findOne({ username });
+      if (existingUser) {
+        return res.status(400).json({ error: "Username đã tồn tại" });
+      }
+
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Create a new account with the role 'Children'
+      const newChildAccount = new Accounts({
+        username,
+        password: hashedPassword,
+        firstName,
+        lastName,
+        image,
+        phone,
+        email,
+        role: 'Children', // Set the role to 'Children'
+      });
+
+      const savedChildAccount = await newChildAccount.save();
+
+      // Create a new Points document for the child account
+      const newPoints = new Points({
+        childId: savedChildAccount._id, // Set the childId to the new account's ID
+        points: 0,
+      });
+
+      // Save the new Points document
+      await newPoints.save();
+
+      res.status(200).json({
+        message: "Tạo tài khoản và điểm cho trẻ em thành công",
+      });
+
+    } catch (err) {
+      next(err)
     }
   }
 
--- routes/accountRouter.js
+++ routes/accountRouter.js
@@ -3,6 +3,7 @@ const accountController = require("../controllers/accountController");
 
 const accountRouter = express.Router();
 accountRouter.route("/user/register").post(accountController.register);
+accountRouter.route("/user/registerChild").post(accountController.registerChild);
 accountRouter.route("/user/login").post(accountController.login);
 
 module.exports = accountRouter;
