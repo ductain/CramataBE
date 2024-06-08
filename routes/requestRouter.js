9db38ab6d658dfe06531682afe413a814e020c10
ductain <taindse170080@fpt.edu.vn>
2024-06-08 22:27:38 +0700

    get request by parent id

--- routes/requestRouter.js
+++ routes/requestRouter.js
@@ -4,6 +4,7 @@ const requestController = require("../controllers/requestController");
 const requestRouter = express.Router();
 
 requestRouter.route("/").get(requestController.getAll);
+requestRouter.route("/requestOfParent").get(requestController.getRequestByParentId);
 requestRouter.route("/create").post(requestController.create);
 
 module.exports = requestRouter;
