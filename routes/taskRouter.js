ae620b839eae515a4322a663b8cc302448f3e084
Nguyen Anh Duy <aduy1912@gmail.com>
2024-06-08 23:24:58 +0700

    fix the fckin silly mistake

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -2,9 +2,12 @@ const express = require("express");
 const taskController = require("../controllers/taskController");
 
 const taskRouter = express.Router();
-taskRouter.route("/").get(taskController.getAllTask);
-taskRouter.route("/").post(taskController.createTask);
+taskRouter.route("/")
+    .get(taskController.getAllTask)
+    .post(taskController.createTask);
+
+taskRouter.route("/parent").get(taskController.getTasksByParentId);
+
 taskRouter.route("/:id").put(taskController.updateTask);
-taskRouter.route("/parent").put(taskController.getTasksByParentId);
 
 module.exports = taskRouter;
