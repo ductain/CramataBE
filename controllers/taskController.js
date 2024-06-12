3a30371f8962090b6897940d6a6e573d96753f39
Nguyen Anh Duy <aduy1912@gmail.com>
2024-06-12 21:21:11 +0700

    add getTaskByChildrenId

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -9,7 +9,7 @@ class TaskController {
       res.status(200).json(data);
     } catch (error) {
       console.log(error);
-      res.status(400).json(error);
+      res.status(400).json({error: error.message});
     }
   }
   async getTasksByParentId(req, res, next) {
@@ -20,7 +20,18 @@ class TaskController {
       res.status(200).json(data);
     } catch (error) {
       console.log(error);
-      res.status(400).json(error);
+      res.status(404).json({ error: "Không tìm thấy nhiệm vụ nào" });
+    }
+  }
+  async getTasksByChildId(req, res, next) {
+    let data = [];
+    const {childId} = req.query;
+    try {
+      data = await Tasks.find({ childId: childId });
+      res.status(200).json(data);
+    } catch (error) {
+      console.log(error);
+      res.status(404).json({ error: "Không tìm thấy nhiệm vụ nào" });
     }
   }
   async createTask(req, res, next) {
@@ -40,7 +51,7 @@ class TaskController {
 
       res.status(201).json({ data: newTask, message: "Nhiệm vụ đã được giao" });
     } catch (err) {
-      next(err);
+      res.status(400).json({ error: "Tạo nhiệm vụ thất bại" });
     }
   }
   async updateTask(req, res, next) {
@@ -70,7 +81,7 @@ class TaskController {
         .status(200)
         .json({ data: updatedTask, message: "Cập nhật nhiệm vụ thành công" });
     } catch (err) {
-      next(err);
+      return res.status(400).json({ error: "Có lỗi xảy ra trong quá trình cập nhật nhiệm vụ" });
     }
   }
 }
